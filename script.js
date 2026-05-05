d585957320a034f9f430311be509f4dfda19271d
Numberone-vic <158623693+Numberone-vic@users.noreply.github.com>
2026-05-04 23:30:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function showMessage(){
-    alert("Welcome to Victor Steele's website!");
+function showMessage() {
+  alert("Welcome to Victor Steele's website!");
 }
-function changeColor(){
-document.body.style.backgroundColor = "#330000";
-}
+
+function changeColor() {
+  document.body.style.backgroundColor = "#330000";
