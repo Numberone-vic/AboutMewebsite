808df81208fb54dfe244562743a5788259dc0fcf
Numberone-vic <158623693+Numberone-vic@users.noreply.github.com>
2026-05-09 21:39:24 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function showMessage() {
-  alert("Welcome to Victor Steele's website!");
+    alert("Welcome to Victor Steele's website!");
 }
 
 function changeColor() {
-  document.body.style.backgroundColor = "#330000";
+    document.body.style.backgroundColor = "#330000";
+}
